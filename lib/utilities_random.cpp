68de0ac0d0468465e771db4ebcfaf1c9492e402d
averydb <averydb@debian.(none)>
2013-02-20 19:27:00 -0500

    legal notice

--- lib/utilities_random.cpp
+++ lib/utilities_random.cpp
@@ -1,3 +1,7 @@
+//          Matthew Avery Coder 2012 - 2013.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
 #include <utilities_random.hpp>
 
 #include <vector>
